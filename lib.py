1b78bd98c4a51dddac781e45231945917fde74c2
Assane DIALLO <assane@pando2.com>
2022-11-07 16:42:13 +0100

    added multi-level game

--- lib.py
+++ lib.py
@@ -18,19 +18,29 @@ def chooseNumber(start, stop):
     return random.randint(start, stop)
 
 
-def getGuessing():
+def getGuessing(remainingAttempts, level):
     try:
         guessing = int(input(
-            '\t- Je viens de penser à un nombre entre 1 et 10. Devinez lequel ?\n\t- Alors mon nombre est : ?\n'))
-        if guessing not in range(1, 11):
+            f'\t- Je viens de penser à un nombre entre 1 et 10. Devinez lequel ?\n\t Il vous reste {remainingAttempts} essai(s) !\n'))
+        if guessing not in range(1, (10*level + 1)):
             guessing = int(
-                input('\t- Je ne comprends pas ! Entrer SVP un nombre entre 1 et 10 :  ?\n'))
+                input(f'\t- Je ne comprends pas ! Entrer SVP un nombre entre 1 et {10*level} :  ?\n'))
             getGuessing()
         return guessing
     except ValueError:
-        print(('\t- Je ne comprends pas ! Entrer SVP un nombre entre 1 et 10 :  ?\n'))
+        print(
+            (f'\t- Je ne comprends pas ! Entrer SVP un nombre entre 1 et {10*level} :  ?\n'))
         getGuessing()
 
 
 def validateGuessing(toGuess, guessed):
-    return toGuess == guessed
+    if guessed > toGuess:
+        return '\t- Votre nombre est trop grand !\n'
+    elif guessed < toGuess:
+        return '\t- Votre nbre est trop petit !\n'
+    else:
+        return guessed == toGuess
+
+
+def pursue():
+    return input('\t- Souhaitez-vous continuer la partie (O/N) ?\n').upper()
--- main.py
+++ main.py
@@ -13,9 +13,42 @@ print('\t\t- de retenter votre chance avec l\'argent qu\'il vous reste pour reco
 print('\t\t- de quitter le jeu.\n')
 print('\t- Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU \n\t\tde continuer le jeu en passant au level supérieur.\n  ')
 
-betted = lib.getBetting()
-toGuess = lib.chooseNumber(1, 10)
-guessed = lib.getGuessing()
-correct = lib.validateGuessing(toGuess, guessed)
 
-print(correct)
+def game(level, attempts, start, stop):
+    betted = lib.getBetting()
+    toGuess = lib.chooseNumber(start, stop)
+    print(toGuess)
+    guessed = lib.getGuessing(attempts, level)
+    guess = lib.validateGuessing(toGuess, guessed)
+
+    gain = 2*betted
+
+    attempts = 1
+    while attempts < 3 and guess != True:
+        guessed = lib.getGuessing(3-attempts)
+        guess = lib.validateGuessing(toGuess, guessed)
+        print(guess)
+        attempts += 1
+
+    # TODO: calculate the gain properly
+
+    if guess == True:
+        print(
+            f'\t- Bingo {pseudo}, vous avez gagné en {attempts} coup(s) et vous avez remporté {gain} € !\n')
+        nextStep = lib.pursue()
+        while nextStep != 'O' and nextStep != 'N':
+            print('\t- Je ne comprends pas votre réponse.')
+            nextStep = lib.pursue()
+        if (nextStep == 'O' and level <= 5):
+            level += 1
+            print(f'\t- Super ! Vous passez au Level {level}.\n')
+            print(
+                f'\t- Rappelez vous, le principe est le même sauf que mon nombre est maintenant entre 1 et {level*10} et\n\t\t vous avez le droit à 5 essais !\n')
+            game(level, 3, 1, level*10)
+        elif nextStep == 'N':
+            print(f'\t- Au revoir ! Vous finissez la partie avec {gain} €.\n ')
+    else:
+        print(f'\t- Vous avez perdu ! Mon nombre était {toGuess} !\n')
+
+
+game(1, 3, 1, 10)
